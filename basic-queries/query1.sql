1657749b3ccfde772b88485e73b5bbef3adc3703
Gio <giovannizavala82@gmail.com>
2025-06-03 15:51:34 -0500

    PAGE 51-52 EXERCISES

--- basic-queries/query1.sql
+++ basic-queries/query1.sql
@@ -1,1 +1,28 @@
-SELECT * from film;
+
+-- q1: products table
+-- q2:
+# USE northwind;
+# SELECT ProductID, ProductName, UnitPrice
+# FROM products;
+-- q3:
+# USE northwind;
+# SELECT ProductID, ProductName, UnitPrice
+# FROM products
+# ORDER BY UnitPrice ASC;
+-- q4:
+# USE northwind;
+# SELECT *
+# FROM products
+# WHERE UnitPrice <= 7.50;
+-- q5:
+# USE northwind;
+# SELECT *
+# FROM products
+# WHERE UnitsInStock >= 100
+# ORDER BY UnitPrice DESC;
+-- q6:
+# USE northwind;
+# SELECT *
+# FROM products
+# WHERE UnitsInStock >= 100
+# ORDER BY UnitPrice DESC, ProductName ASC;
