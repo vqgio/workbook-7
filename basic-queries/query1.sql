5d2193723e2049fb44abdf84bb86a49b92c902ac
Gio <giovannizavala82@gmail.com>
2025-06-04 12:01:34 -0500

    PAGE 51-52 EXERCISES

--- basic-queries/query1.sql
+++ basic-queries/query1.sql
@@ -26,3 +26,34 @@
 # FROM products
 # WHERE UnitsInStock >= 100
 # ORDER BY UnitPrice DESC, ProductName ASC;
+-- q7:
+# USE northwind;
+# SELECT *
+# FROM products
+# WHERE UnitsInStock = 0
+# AND UnitsOnOrder > 1
+# ORDER BY ProductName ASC;
+-- q8:
+# USE northwind;
+# SELECT categories.CategoryName
+# FROM categories;
+-- q9:
+# USE northwind;
+# SELECT *
+# FROM categories;
+-- Seafood cagetoryid is 8.
+-- q10:
+# USE northwind;
+# SELECT *
+# FROM products
+# WHERE CategoryID = 8;
+-- q11:
+# USE northwind;
+# SELECT employees.FirstName, employees.LastName
+# FROM employees;
+-- q12:
+# USE northwind;
+# SELECT *
+# FROM employees
+# WHERE Title = 'Sales Manager';
+
